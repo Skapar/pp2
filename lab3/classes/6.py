c6f245afdb0cefa3558f3522f3ac76eb41308c49
Skapar <skaparadr@gmail.com>
2023-02-19 02:23:58 +0400

    commit

--- lab3/classes/6.py
+++ lab3/classes/6.py
@@ -1,5 +1,7 @@
-
-def fillt(listik):
-    print(list(filter(lambda x : all( x % i !=0  
+class filter:
+    def __init__(self):
+        pass
+    def fillt(listik):
+        print(list(filter(lambda x : all( x % i !=0  
                                      for i in range(2,x)),listik)))  
 # fillt([3, 5, 8, 15, 7])
