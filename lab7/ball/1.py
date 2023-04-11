288c95b8078715f86dd64b39cc63b0f5ce7a74a9
Skapar <skaparadr@gmail.com>
2023-04-11 23:43:09 +0400

    commit

--- lab7/ball/1.py
+++ lab7/ball/1.py
@@ -1,37 +1,42 @@
 import pygame
-import sys
- 
-FPS = 60
-W = 700 
-H = 300 
-WHITE = (255, 255, 255)
-BLUE = (0, 70, 225)
-RED = (139, 0, 0)
-sc = pygame.display.set_mode((W, H))
-clock = pygame.time.Clock()
- 
 
-x = W // 2
-y = H // 2
-r = 25
- 
-while 1:
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            sys.exit()
-        elif i.type == pygame.KEYDOWN:
-            if i.key == pygame.K_LEFT:
-                x -= 5
-            elif i.key == pygame.K_RIGHT:
-                x += 5
-            elif i.key == pygame.K_DOWN:
-                y += 5
-            elif i.key == pygame.K_UP:
-                y -= 5 
-            
- 
-    sc.fill(WHITE)
-    pygame.draw.circle(sc, RED, (x, y), r)
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Ball")
+
+ball_color = (255, 0, 0)
+ball_radius = 25
+ball_position = [screen.get_width() // 2, screen.get_height() // 2]
+ball_speed = 20
+
+def draw_ball():
+    pygame.draw.circle(screen, ball_color, ball_position, ball_radius)
+
+running = True
+while running:
+    screen.fill((255, 255, 255))
+
+    draw_ball()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                ball_position[1] -= ball_speed
+            elif event.key == pygame.K_DOWN:
+                ball_position[1] += ball_speed
+            elif event.key == pygame.K_LEFT:
+                ball_position[0] -= ball_speed
+            elif event.key == pygame.K_RIGHT:
+                ball_position[0] += ball_speed
+
+    ball_position[0] = max(ball_position[0], ball_radius)
+    ball_position[0] = min(ball_position[0], screen.get_width() - ball_radius)
+    ball_position[1] = max(ball_position[1], ball_radius)
+    ball_position[1] = min(ball_position[1], screen.get_height() - ball_radius)
+
     pygame.display.update()
-    clock.tick(FPS)
- 
+
+pygame.quit()
--- lab7/musicplayer.py
+++ lab7/musicplayer.py
@@ -1,21 +1,33 @@
 import pygame
 
-pygame.mixer.init()
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+
+pygame.display.set_caption("Music Player")
 
 pygame.mixer.music.load("song.mp3")
-
-pygame.mixer.music.set_volume(0.7)
 
 pygame.mixer.music.play()
 
 while True:
-    q = input()
-    if q == 'p' :
-        pygame.mixer.pause()
-    elif q == 'r':
-        pygame.mixer.unpause()
-    elif q == 's':
-        pygame.mixer.stop()
-        break
-clock = pygame.time.clock()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                pygame.mixer.music.unpause()
+            elif event.key == pygame.K_s:
+                pygame.mixer.music.stop()
+            elif event.key == pygame.K_n:
+                pygame.mixer.music.load("song.mp3")
+                pygame.mixer.music.play()
+            elif event.key == pygame.K_b:
+                pygame.mixer.music.load("song.mp3")
+                pygame.mixer.music.play()
+            elif event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                quit()
 
+    pygame.display.update()
